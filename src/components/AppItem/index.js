3e7ee4924a3a8a11ff54aa8b12f4e7945808f14e
Sahana <sahanas012@gmail.com>
2022-09-01 21:51:38 +0000

    App Store

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+import './index.css'
+
+const AppItem = props => {
+  const {appDetails} = props
+  const {appName, imageUrl} = appDetails
+
+  return (
+    <>
+      <li className="app-item-container ">
+        <img className="app-item-image" src={imageUrl} alt={appName} />
+        <p className="app-item-title">{appName}</p>
+      </li>
+    </>
+  )
+}
+
+export default AppItem
